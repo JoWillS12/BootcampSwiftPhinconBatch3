ca9ca1aca84b28df0293c3dd9e4a94ebc4bc77b0
Joseph William Santoso <jowills.1012@gmail.com>
2023-10-24 16:20:05 +0700

    Day 2 Finished

--- Academic/Week1/Day 2/Case/Day2Case.playground/Contents.swift
+++ Academic/Week1/Day 2/Case/Day2Case.playground/Contents.swift
@@ -9,29 +9,34 @@ class ATMmachine: WelcomeMessage {
     enum Menu: Int {
         case Deposit = 1, Withdraw, Balance, Exit
     }
+    enum ErrorPing: Error{
+        case Limit
+    }
     
     var originPass = 13579
     var depo: Int
     var balance: Int
     private var _wDraw: Int = 0
-    var wDraw: Int{
-        get {
-            return _wDraw
-        }
-        set {
-            if newValue <= balance {
-                _wDraw = newValue
-            }
-        }
-    }
+    var wDraw: Int
+//    {
+//        get {
+//            return _wDraw
+//        }
+//        set {
+//            if newValue <= balance {
+//                _wDraw = newValue
+//            }
+//        }
+//    }
     
     init() {
         depo = 500
         balance = 2000
+        wDraw = 300
     }
 }
 
-extension ATMmachine{
+extension ATMmachine {
     func welcome() {
         print("===== WELCOME USER =====")
         print("========================")
@@ -43,7 +48,11 @@ extension ATMmachine{
             case .Deposit:
                 add()
             case .Withdraw:
-                min()
+                do {
+                    try min()
+                } catch {
+                    print("Insufficient balance for withdrawal")
+                }
             case .Balance:
                 check()
             case .Exit:
@@ -64,27 +73,34 @@ extension ATMmachine{
         print("YOUR BALANCE IS $\(balance)")
     }
     
-    func min() {
+    func min() throws {
         if balance >= wDraw {
             print("YOU WITHDREW $\(wDraw)")
             balance -= wDraw
             print("YOUR BALANCE IS $\(balance)")
         } else {
-            print("Insufficient balance for withdrawal.")
+            throw ErrorPing.Limit
         }
     }
+    
+//    func min() throws {
+//        if balance >= wDraw {
+//            print("YOU WITHDREW $\(wDraw)")
+//            balance -= wDraw
+//            print("YOUR BALANCE IS $\(balance)")
+//        } else {
+//            throw NSError(domain: "ATMErrorDomain", code: 1, userInfo: ["message": "Insufficient balance for withdrawal"])
+//        }
+//    }
     
     func exit() {
         print("======= THANKYOU =======")
     }
-    
 }
 
-
 let a = ATMmachine()
-a.wDraw = 100
+//a.wDraw = 5000
 a.welcome()
-a.detectInput(menu: 2)
-
+a.detectInput(menu: 2) // Attempting a withdrawal
 
 
