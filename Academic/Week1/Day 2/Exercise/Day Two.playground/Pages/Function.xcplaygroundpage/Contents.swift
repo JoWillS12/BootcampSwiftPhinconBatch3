2751b6530808b9bc941d4eb6249f314658cbc4a6
Joseph William Santoso <jowills.1012@gmail.com>
2023-10-24 11:44:57 +0700

    Function Struct and Closure

--- Academic/Week1/Day 2/Exercise/Day Two.playground/Pages/Function.xcplaygroundpage/Contents.swift
+++ Academic/Week1/Day 2/Exercise/Day Two.playground/Pages/Function.xcplaygroundpage/Contents.swift
@@ -10,8 +10,6 @@ func greet(name: String) {
 }
 
 greet(name: "Alice")
-greet(name: "Bob")
-// The 'greet' function takes a 'name' parameter and prints a greeting with the provided name.
 
 // Example 2: Function with Return Value
 // Functions can also return values.
@@ -22,7 +20,6 @@ func square(_ number: Int) -> Int {
 
 let result = square(5)
 print("Square of 5 is \(result)")
-// The 'square' function takes an integer 'number' and returns its square.
 
 // Example 3: Function with Multiple Parameters
 // Functions can have multiple parameters.
@@ -33,34 +30,9 @@ func calculateSum(of a: Int, and b: Int) -> Int {
 
 let sum = calculateSum(of: 3, and: 4)
 print("The sum of 3 and 4 is \(sum)")
-// The 'calculateSum' function takes two integers and returns their sum.
 
-// Example 4: Function with Default Parameter Value
-// Default parameter values can be provided for function parameters.
 
-func power(_ base: Int, to exponent: Int = 2) -> Int {
-    return Int(pow(Double(base), Double(exponent)))
-}
-
-let squareResult = power(4)
-let cubeResult = power(3, to: 3)
-print("4 squared is \(squareResult), 3 cubed is \(cubeResult)")
-// The 'power' function calculates the power of a number with an optional exponent (defaulting to 2).
-
-// Example 5: Function with Variadic Parameters
-// Variadic parameters allow a variable number of arguments.
-
-func average(_ numbers: Double...) -> Double {
-    let sum = numbers.reduce(0, +)
-    return sum / Double(numbers.count)
-}
-
-let avg1 = average(1, 2, 3, 4, 5)
-let avg2 = average(10, 20, 30)
-print("Average 1: \(avg1), Average 2: \(avg2)")
-// The 'average' function calculates the average of a variable number of double values.
-
-// Example 6: Function with In-Out Parameters
+// Example 4: Function with In-Out Parameters
 // In-out parameters allow a function to modify the value of an external variable.
 
 func swapValues(_ a: inout Int, _ b: inout Int) {
@@ -72,21 +44,8 @@ func swapValues(_ a: inout Int, _ b: inout Int) {
 var x = 5, y = 10
 swapValues(&x, &y)
 print("After swapping: x = \(x), y = \(y)")
-// The 'swapValues' function swaps the values of two integers using in-out parameters.
 
-// Example 7: Function as a Parameter
-// Functions can be passed as parameters to other functions.
-
-func applyOperation(_ a: Int, _ b: Int, operation: (Int, Int) -> Int) -> Int {
-    return operation(a, b)
-}
-
-let additionResult = applyOperation(3, 4, operation: { (a, b) in a + b })
-let multiplicationResult = applyOperation(2, 5, operation: { (a, b) in a * b })
-print("Addition Result: \(additionResult), Multiplication Result: \(multiplicationResult)")
-// The 'applyOperation' function takes two integers and a function as a parameter to perform the operation.
-
-// Example 8: Nested Functions
+// Example 5: Nested Functions
 // Functions can be defined within other functions.
 
 func performOperation(_ operation: String) -> (Int, Int) -> Int {
@@ -110,9 +69,8 @@ let multiplicationFunction = performOperation("multiply")
 let result1 = additionFunction(3, 4)
 let result2 = multiplicationFunction(3, 4)
 print("Result 1: \(result1), Result 2: \(result2)")
-// The 'performOperation' function returns a nested function based on the operation type.
 
-// Example 9: Function with a Closure
+// Example 6: Function with a Closure
 // Functions can accept closures (anonymous functions) as arguments.
 
 func processNumbers(_ numbers: [Int], using closure: (Int) -> Int) {
@@ -124,25 +82,29 @@ let numbers = [1, 2, 3, 4, 5]
 processNumbers(numbers) { number in
     return number * 2
 }
-// The 'processNumbers' function applies the closure to each number in the array and prints the results.
 
-
-// Example 10: Function with Enum
-enum Hari: String{
+// Example 7: Function with Enum and rawValue
+enum Hari: String {
     case Senin, Selasa, Rabu, Kamis, Jumat
     case Sabtu, Minggu
 }
 
-func detectHolidays(hari: Hari)-> String{
-    switch hari {
-    case .Senin, .Selasa, .Rabu, .Kamis, .Jumat:
-        return("WeekDay.")
-    case .Sabtu, .Minggu:
-        return("Holoday.")
+func detectHolidays(hari: String) -> String {
+    if let day = Hari(rawValue: hari) {
+        switch day {
+        case .Senin, .Selasa, .Rabu, .Kamis, .Jumat:
+            return "Weekday."
+        case .Sabtu, .Minggu:
+            return "Holiday."
+        }
+    } else {
+        return "Invalid day."
     }
 }
 
-detectHolidays(hari: .Senin)
+let dayResult = detectHolidays(hari: "Senin")
+print(dayResult) // Output: Weekday.
+
 
 
 //: [Next](@next)
--- Academic/Week1/Day 2/Exercise/Day Two.playground/Pages/Struct.xcplaygroundpage/Contents.swift
+++ Academic/Week1/Day 2/Exercise/Day Two.playground/Pages/Struct.xcplaygroundpage/Contents.swift
@@ -2,6 +2,53 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+// Struct Example 1
+
+struct Person {
+    var name: String
+    var age: Int
+    var friend: [Friends]
+}
+
+struct Friends{
+    var name: String
+    var age: Int
+}
+
+var friendList = Friends(name: "Chris", age: 20)
+var personOne = Person(name: "John", age: 17, friend: [friendList])
+
+
+print("Person's name is \(personOne.name), he's \(personOne.age) years old and has friend named \(friendList.name) and he's \(friendList.age) years old.")
+
+// Struct Example 2
+
+struct TrafficLight {
+    enum State {
+        case red
+        case yellow
+        case green
+    }
+    
+    var currentState: State
+    
+    func nextLight() -> TrafficLight {
+        switch currentState {
+        case .red:
+            return TrafficLight(currentState: .green)
+        case .yellow:
+            return TrafficLight(currentState: .red)
+        case .green:
+            return TrafficLight(currentState: .yellow)
+        }
+    }
+}
+
+var trafficLight = TrafficLight(currentState: .red)
+print("Current state: \(trafficLight.currentState)")
+
+trafficLight = trafficLight.nextLight()
+print("Changed state: \(trafficLight.currentState)")
+
 
 //: [Next](@next)
